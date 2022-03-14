a5ad13c091e6db6106567a186e0c42e6bae4cb53
ORIENTSOFTWARE\hieu.trantrinh <hieu.trantrinh@orientsoftware.com>
2022-03-14 18:21:47 +0700

    animation and responsive matches page

---
+++ src/Pages/Matches/components/MatchItem.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Col } from "antd";
+
+import PremierLeague from "../../../Assets/Images/Premier-League.png";
+
+type Props = {};
+
+const MatchItem = (props: Props) => {
+  return (
+    <Col xxl={8} xl={12} lg={24} md={12} sm={24} xs={24} >
+      <div className="match-item">
+        <Link to="/matches/abc" className="match-item__link" />
+        <div className="match-item__league">
+          <i className="match-item__league--icon">
+            <img src={PremierLeague} alt="logo" />
+          </i>
+          UEFA Europa Conference League
+        </div>
+        <div className="match-item__team match-item__team--home">
+          <div className="match-item__team--logo">
+            <img
+              loading="lazy"
+              alt="team"
+              src="https://api.sofascore.com/api/v1/team/2958/image"
+            />
+          </div>
+          <h3 className="match-item__team--name">Vitesse</h3>
+        </div>
+        <div className="match-item__info">
+          <div className="match-item__info--time">
+            <span>13:00</span>
+          </div>
+          <div className="match-item__info--date">
+            <span>11/03/2022</span>
+          </div>
+        </div>
+        <div className="match-item__team match-item__team--away">
+          <div className="match-item__team--logo">
+            <img
+              loading="lazy"
+              alt="team"
+              src="https://api.sofascore.com/api/v1/team/2702/image"
+            />
+          </div>
+          <h3 className="match-item__team--name">Roma</h3>
+        </div>
+      </div>
+    </Col>
+  );
+};
+
+export default MatchItem;
